81024d4085a3d9c7f836abbff43a020fc2f40251
Malav <malavp769@gmail.com>
2018-02-28 00:51:52 -0400

    Location Reminder - Layout File

---
+++ app/src/main/java/due2do/mobile/com/duetodo/create2.java
@@ -0,0 +1,28 @@
+package due2do.mobile.com.duetodo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class create2 extends AppCompatActivity {
+
+    Button addtask;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create2);
+
+        addtask = (Button) findViewById(R.id.button2);
+
+        addtask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(create2.this, to_do.class));
+            }
+        });
+
+    }
+}
