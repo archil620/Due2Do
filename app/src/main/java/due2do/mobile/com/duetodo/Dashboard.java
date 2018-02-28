6912682c55bc38ce0bd4bf27544583653b1e8921
Bhargav Dalal <dalal.bhargav07@gmail.com>
2018-02-28 12:04:24 -0400

    Authentication Done, Sign Out Functionality added

--- app/src/main/java/due2do/mobile/com/duetodo/Dashboard.java
+++ app/src/main/java/due2do/mobile/com/duetodo/Dashboard.java
@@ -1,7 +1,10 @@
 package due2do.mobile.com.duetodo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -10,15 +13,26 @@ import com.google.firebase.auth.FirebaseUser;
 public class Dashboard extends AppCompatActivity {
 
     private TextView aname;
+    private Button signOutBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        signOutBtn = (Button) findViewById(R.id.sout);
         aname = (TextView) findViewById(R.id.authname);
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String name = user.getDisplayName();
         aname.setText("Welcome " + name);
+
+        signOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(Dashboard.this, MainActivity.class));
+            }
+        });
     }
 }
