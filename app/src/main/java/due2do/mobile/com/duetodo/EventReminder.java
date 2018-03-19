f8a998cd210626a5f6d45cedffad4933d8e8d07c
abhispatel42 <abhipatel30@yahoo.com>
2018-03-19 00:56:58 -0300

    Date and time setter commit

---
+++ app/src/main/java/due2do/mobile/com/duetodo/EventReminder.java
@@ -0,0 +1,62 @@
+package due2do.mobile.com.duetodo;
+
+/**
+ * Created by Abhi on 2018-03-19.
+ */
+
+public class EventReminder {
+    private String task, hour, minute, year, month, day, contact;
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    public String getMinute() {
+        return minute;
+    }
+
+    public void setMinute(String minute) {
+        this.minute = minute;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getContact() {return contact;}
+
+    public void setContact(String contact) {this.contact = contact;}
+
+}
