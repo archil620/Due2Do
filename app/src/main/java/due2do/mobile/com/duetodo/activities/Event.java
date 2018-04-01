a57b4adf42acc1fd2c68269a459d3938098f4eca
abhispatel42 <abhipatel30@yahoo.com>
2018-04-01 15:25:33 -0300

    Select contact and send message - Event Reminder

--- app/src/main/java/due2do/mobile/com/duetodo/activities/Event.java
+++ app/src/main/java/due2do/mobile/com/duetodo/activities/Event.java
@@ -88,7 +88,102 @@ public class Event extends AppCompatActivity implements DatePickerDialog.OnDateS
                 timePickerDialog.show();
             }
         });
+
+        btPick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+
+
+        createtask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (location.getText().toString().length() != 0 && eventName.getText().toString().length() != 0) {
+                    //String phoneNo =txtPhoneNo.getText().toString();
+
+                    for (int i = 0; i < contactList.size(); i++) {
+                        String phoneNo = contactList.get(i);
+                        String message = "You have been added to an event named " + eventName.getText().toString() + " on " +
+                                eventReminder.getDay() + "/" + eventReminder.getMonth() + "/" + eventReminder.getYear() + "at " + eventReminder.getHour() + ":" + eventReminder.getMinute() + ".The vemue is " +
+                                location.getText().toString();
+                        if (adapter != null) {
+                            mDatabaseReference.child(mUser.getUid()).child("EventTask").push().setValue(eventReminder);
+                            sendMessage(phoneNo, message);
+
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Phone or message field is empty", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                } else {
+                    Toast.makeText(getApplicationContext(), "Enter all fields", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+    public void sendMessage(String phoneNo, String message) {
+        //https://stackoverflow.com/questions/40861846/android-get-phone-number-from-contact-list
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage(phoneNo, null, message, null, null);
+            Toast.makeText(getApplicationContext(), "SMS Sent", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getApplicationContext(), "MSG Failed", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int reqCode, int resultCode, Intent data) {
+        int flag = 0;
+        //http://www.worldbestlearningcenter.com/tips/Android-get-phone-number-from-contacts-list.htm
+        super.onActivityResult(reqCode, resultCode, data);
+        switch (reqCode) {
+            case (REQUEST_CODE):
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri contactData = data.getData();
+                    Cursor c = getContentResolver().query(contactData, null, null, null, null);
+                    if (c.moveToFirst()) {
+                        String contactId = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+                        String hasNumber = c.getString(c.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                        String num = "";
+                        if (Integer.valueOf(hasNumber) == 1) {
+                            Cursor numbers = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = " + contactId, null, null);
+                            while (numbers.moveToNext()) {
+                                num = numbers.getString(numbers.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                                {
+                                    Uri uri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(num));
+
+                                    String[] projection = new String[]{ContactsContract.PhoneLookup.DISPLAY_NAME};
+
+                                    String contactName = "";
+                                    Cursor cursor = this.getContentResolver().query(uri, projection, null, null, null);
+
+                                    if (cursor != null) {
+                                        if (cursor.moveToFirst()) {
+                                            contactName = cursor.getString(0);
+                                            Log.i("name", contactName);
+                                            if (flag == 0) {
+                                                contactList.add(contactName);
+                                                adapter.notifyDataSetChanged();
+                                                flag = 1;
+                                            }
+                                        }
+                                        cursor.close();
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    break;
+                }
+        }
+    }
+
 
     @Override
     public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
