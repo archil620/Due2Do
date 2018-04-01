8f9e0b2f86c4e9aa030fb23cf847c764683e9b8b
Rahul <rahulvala1502@gmail.com>
2018-04-01 15:10:40 -0300

    Created locationModel

---
+++ app/src/main/java/due2do/mobile/com/duetodo/model/LocationModel.java
@@ -0,0 +1,101 @@
+package due2do.mobile.com.duetodo.model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+/**
+ * Created by rahul on 2018-04-01.
+ */
+
+public class LocationModel {
+
+    private String taskName;
+    private String id;
+    private double latitude;
+    private double longitude;
+    private String taskStatus;
+    private String year;
+    private String month;
+    private String day;
+    private String hour;
+    private String minute;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public String getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(String taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public void setMonth(String month) {
+        this.month = month;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour;
+    }
+
+    public String getMinute() {
+        return minute;
+    }
+
+    public void setMinute(String minute) {
+        this.minute = minute;
+    }
+}
