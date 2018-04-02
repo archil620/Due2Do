9210c1fab38665ec3580dfd4ea219ed2586559be
Bhargav Dalal <dalal.bhargav07@gmail.com>
2018-04-02 02:59:10 -0300

    OnClick app broadcast

---
+++ app/src/main/java/due2do/mobile/com/duetodo/services/AppBroadcastReceiver.java
@@ -0,0 +1,24 @@
+package due2do.mobile.com.duetodo.services;
+
+import android.app.NotificationManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Bhargav Dalal on 18/03/2018.
+ */
+
+public class AppBroadcastReceiver extends BroadcastReceiver {
+    private static final String TAG = "intenttest.mc.com.intenttest";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent stopIntent = new Intent(context, RingtonePlayingService.class);
+        context.stopService(stopIntent);
+
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        manager.cancel(101);
+
+    }
+}
